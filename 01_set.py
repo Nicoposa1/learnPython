f9183bef3e01b14fc23220117b21e32237923b2b
Nicolas Posa <nicoposa57@gmail.com>
2023-12-25 04:05:08 -0300

    first commit

---
+++ 01_set.py
@@ -0,0 +1,17 @@
+set_countries = {"USA", "China", "Japan"}
+print(set_countries)
+print(type(set_countries))
+
+set_numbers = {1, 2, 3, 4, 5}
+print(set_numbers)
+
+set_mixed = {1, 2, 3, "USA", "China", "Japan", False}
+print(set_mixed)
+print(type(set_mixed))
+
+set_from_string = set("Hello")
+print(set_from_string)
+
+set_from_tuple = set((1, 2, 3, 4, 5))
+print(set_from_tuple)
+print(type(set_from_tuple))
