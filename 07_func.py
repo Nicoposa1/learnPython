e419666d736f9cf7abc5320f339677459dd16ec9
Nicolas Posa <nicoposa57@gmail.com>
2023-12-26 01:29:26 -0300

    functions

---
+++ 07_func.py
@@ -0,0 +1,25 @@
+print('Hola')
+
+
+def my_print():
+    print('This is my print function')
+
+
+my_print()
+
+
+def my_print2(text):
+    print(text)
+
+
+my_print2('params')
+
+a = 10
+b = 20
+
+
+def suma(a, b):
+    print(a + b)
+
+
+suma(19, 28)
