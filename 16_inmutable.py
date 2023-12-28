21eae348aa9e5b8374124fe08d7914aa1ba55835
Nicolas Posa <nicoposa57@gmail.com>
2023-12-28 12:16:09 -0300

    inmutable list

---
+++ 16_inmutable.py
@@ -0,0 +1,33 @@
+items = [
+    {
+        "id": 1,
+        "name": "laptop",
+        "price": 100,
+    },
+    {
+        "id": 2,
+        "name": "mouse",
+        "price": 40,
+    },
+    {
+        "id": 3,
+        "name": "monitor",
+        "price": 400,
+    },
+]
+
+prices = list(map(lambda item: item['name'], items))
+print(prices)
+
+
+def add_taxes(item):
+    new_item = item.copy()
+    new_item['taxes'] = item['price'] * 0.19
+    return new_item
+
+
+new_items = list(map(add_taxes, items))
+print('new_items')
+print(new_items)
+print('old list')
+print(items)
