ff6c7014f60dcaa6946502f82bd92d3b46f46efc
Nicolas Posa <nicoposa57@gmail.com>
2023-12-31 06:08:24 -0300

    packages

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import this
+import pkg
+# from pkg.mod_1 import func1, func2
+
+# print(func1())
+# print(func2())
+
+print(pkg.URL)
+print(pkg.mod_1.func1())
---
+++ pkg/__init__.py
@@ -0,0 +1,4 @@
+import pkg.mod_2
+import pkg.mod_1
+print('se inicio el paquete')
+URL = 'nicoposa.com'
