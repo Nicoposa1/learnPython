923ad614be68b52bb57b1d438915e1a322c33618
Nicolas Posa <nicoposa57@gmail.com>
2024-01-06 20:23:32 -0300

    test

--- app/utils.py
+++ app/utils.py
@@ -15,7 +15,6 @@ def get_population(country_dict):
 
 
 def get_world_percentage(data):
-    # {'Rank': '36', 'CCA3': 'AFG', 'Country/Territory': 'Afghanistan', 'Capital': 'Kabul', 'Continent': 'Asia', '2022 Population': '41128771', '2020 Population': '38972230', '2015 Population': '33753499', '2010 Population': '28189672', '2000 Population': '19542982', '1990 Population': '10694796', '1980 Population': '12486631', '1970 Population': '10752971', 'Area (km²)': '652230', 'Density (per km²)': '63.0587', 'Growth Rate': '1.0257', 'World Population Percentage': '0.52'}
     total_population = 0
     for country in data:
         total_population += int(country['2022 Population'])
@@ -24,6 +23,7 @@ def get_world_percentage(data):
             (int(country['2022 Population']) / total_population) * 100, 2)
         country['World Population Percentage'] = percentage
     return data
+
 
 
 def population_by_country(data, country):
